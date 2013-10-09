a260080fe10ddaefe7cbdcd3ea9166381bdf9abd
Sedclerk <seclerk@gmail.com>
2013-10-09 17:07:38 +0300

    9th October 2013

--- project_18/db/schema.rb
+++ project_18/db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131009075207) do
+
+  create_table "visitors", force: true do |t|
+    t.string   "name",        limit: 60
+    t.text     "description"
+    t.date     "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
