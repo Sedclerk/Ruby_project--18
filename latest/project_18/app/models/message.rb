55fd61f433318732dc49bde689a2a09ca0a55014
Sedclerk <seclerk@gmail.com>
2013-10-21 17:17:11 +0300

    deleted

--- latest/project_18/app/models/message.rb
+++
@@ -1,9 +0,0 @@
-class Message < ActiveRecord::Base
-	validates_presence_of :first_name, :last_name, :email, :phone, :content
-	validates_length_of :first_name, :within => 3..25
-    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-    validates_format_of :phone,
-      :with => /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
-      :message => "- Phone numbers must be in xxx-xxx-xxxx format."
-
-end
