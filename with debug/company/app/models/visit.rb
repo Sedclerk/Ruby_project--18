a7518ebdc7772768eb67195b3c9ab7570e72fe10
Sedclerk <seclerk@gmail.com>
2013-11-26 11:55:11 +0300

    debug

---
+++ with debug/company/app/models/visit.rb
@@ -0,0 +1,17 @@
+class Visit < ActiveRecord::Base
+
+belongs_to :visitable, :polymorphic => true
+  has_many :visit_stats
+
+	def self.track(obj, ip_address)
+    visit = Visit.find_or_create_by_visit_id(obj.id)
+    
+    unless VisitStat.find_by_visit_id_and_ip_address(visit.id, ip_address)
+         #visit_stat.increment!(:num_of_visits)
+    end
+    
+    #visit.increment!(:num_of_visits)
+    visit_stat = visit.visit_stats.create(:ip_address => ip_address)
+  
+  end
+end
